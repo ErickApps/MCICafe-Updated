fde9ba82bbc5e3ad89a51e5f32ee47e791049ba0
Erick Barbosa <Erick@Ericks-iMac.local>
2017-01-10 23:58:20 -0500

    added go to facebook etc..

--- MCICafe/InfoViewController.swift
+++ MCICafe/InfoViewController.swift
@@ -38,11 +38,44 @@ class InfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func goToMdcButton(_ sender: AnyObject) {
+        let url = URL(string: "http://www.miamidadeculinary.com")
+        goToURL(url: url!)
+    }
+    @IBAction func phoneButton(_ sender: UIButton) {
+        
+         let number = URL(string: "telprompt://3052373276" )
+        
+        goToURL(url: number!)
+    }
+    @IBAction func facebookWebButton(_ sender: UIButton) {
+        let url = URL(string: "https://www.facebook.com/MiamiCulinaryInstitute")
+        goToURL(url: url!)
+    }
+    
+    @IBAction func twitterWebButton(_ sender: UIButton) {
+        let url = URL(string: "https://twitter.com/MDC_Culinary")
+        goToURL(url: url!)
+    }
+    
+    @IBAction func youtubeWebButton(_ sender: UIButton) {
+        let url = URL(string: "https://www.youtube.com/user/MiamiDadeCulinary")
+        goToURL(url: url!)
+
+    }
+    @IBAction func instangramWebButton(_ sender: UIButton) {
+        let url = URL(string: "https://instagram.com/mcimedia")
+        goToURL(url: url!)
+        
+    }
     
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
                                                                   regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    func goToURL(url: URL) {
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
     
     
