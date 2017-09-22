3ca8fafe258edb44958ae6dbce0a6b2ad628af70
Erick <pitjitsu@hotmail.com>
2017-09-21 20:30:35 -0400

    rectoring

--- MCICafe/AppDelegate.swift
+++ MCICafe/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  MCICafe
 //
-//  Created by Erick Barbosa on 12/23/16.
+//  Created by Erick Barbosa on 12/23/16
 //  Copyright © 2016 Erick Barbosa. All rights reserved.
 //
 
@@ -15,157 +15,157 @@ import FirebaseInstanceID
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  let gcmMessageIDKey = "gcm.message_id"
+  
+  
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    var window: UIWindow?
-    let gcmMessageIDKey = "gcm.message_id"
-    
-    
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        // Register for remote notifications. This shows a permission dialog on first run, to
-        // show the dialog at a more appropriate time move this registration accordingly.
-        // [START register_for_notifications]
-        if #available(iOS 10.0, *) {
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-            
-            // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
-            // For iOS 10 data message (sent via FCM)
-            FIRMessaging.messaging().remoteMessageDelegate = self
-            
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
-        
-        application.registerForRemoteNotifications()
-        
-        
-        // [END register_for_notifications]
-        
-        FIRApp.configure()
-        
-        ///check if data persistence is working properly with sync
-        
-        FIRDatabase.database().persistenceEnabled = true
-        let menuRef = FIRDatabase.database().reference(withPath: "menu")
-        menuRef.keepSynced(true)
-        
-        
-        // Add observer for InstanceID token refresh callback.
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(self.tokenRefreshNotification),
-                                               name: .firInstanceIDTokenRefresh,
-                                               object: nil)
-        
-        
-        
-        return true
+    // Register for remote notifications. This shows a permission dialog on first run, to
+    // show the dialog at a more appropriate time move this registration accordingly.
+    // [START register_for_notifications]
+    if #available(iOS 10.0, *) {
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: {_, _ in })
+      
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
+      // For iOS 10 data message (sent via FCM)
+      FIRMessaging.messaging().remoteMessageDelegate = self
+      
+    } else {
+      let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
     }
     
-    // [START receive_message]
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-        
-        
+    application.registerForRemoteNotifications()
+    
+    
+    // [END register_for_notifications]
+    
+    FIRApp.configure()
+    
+    ///check if data persistence is working properly with sync
+    
+    FIRDatabase.database().persistenceEnabled = true
+    let menuRef = FIRDatabase.database().reference(withPath: "menu")
+    menuRef.keepSynced(true)
+    
+    
+    // Add observer for InstanceID token refresh callback.
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(self.tokenRefreshNotification),
+                                           name: .firInstanceIDTokenRefresh,
+                                           object: nil)
+    
+    
+    
+    return true
+  }
+  
+  // [START receive_message]
+  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+    
+    
+  }
+  
+  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                   fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    
+    completionHandler(UIBackgroundFetchResult.newData)
+  }
+  // [END receive_message]
+  
+  // [START refresh_token]
+  func tokenRefreshNotification(_ notification: Notification) {
+    if let refreshedToken = FIRInstanceID.instanceID().token() {
+      print("InstanceID token: \(refreshedToken)")
     }
     
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        
-        completionHandler(UIBackgroundFetchResult.newData)
-    }
-    // [END receive_message]
-    
-    // [START refresh_token]
-    func tokenRefreshNotification(_ notification: Notification) {
-        if let refreshedToken = FIRInstanceID.instanceID().token() {
-            print("InstanceID token: \(refreshedToken)")
-        }
-        
-        // Connect to FCM since connection may have failed when attempted before having a token.
-        connectToFcm()
-    }
-    // [END refresh_token]
-    
-    // [START connect_to_fcm]
-    func connectToFcm() {
-        // Won't connect since there is no token
-        guard FIRInstanceID.instanceID().token() != nil else {
-            return;
-        }
-        
-        // Disconnect previous FCM connection if it exists.
-        FIRMessaging.messaging().disconnect()
-        
-        FIRMessaging.messaging().connect { (error) in
-            if error != nil {
-                
-            } else {
-                
-            }
-        }
-    }
-    // [END connect_to_fcm]
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("Unable to register for remote notifications: \(error.localizedDescription)")
+    // Connect to FCM since connection may have failed when attempted before having a token.
+    connectToFcm()
+  }
+  // [END refresh_token]
+  
+  // [START connect_to_fcm]
+  func connectToFcm() {
+    // Won't connect since there is no token
+    guard FIRInstanceID.instanceID().token() != nil else {
+      return;
     }
     
-    // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
-    // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
-    // the InstanceID token.
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        FIRMessaging.messaging().subscribe(toTopic: "/topics/notification")
-        
-    }
+    // Disconnect previous FCM connection if it exists.
+    FIRMessaging.messaging().disconnect()
     
-    // [START connect_on_active]
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        connectToFcm()
-    }
-    // [END connect_on_active]
-    
-    // [START disconnect_from_fcm]
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        FIRMessaging.messaging().disconnect()
+    FIRMessaging.messaging().connect { (error) in
+      if error != nil {
         
+      } else {
+        
+      }
     }
-    // [END disconnect_from_fcm]
+  }
+  // [END connect_to_fcm]
+  
+  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    print("Unable to register for remote notifications: \(error.localizedDescription)")
+  }
+  
+  // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
+  // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
+  // the InstanceID token.
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    FIRMessaging.messaging().subscribe(toTopic: "/topics/notification")
+    
+  }
+  
+  // [START connect_on_active]
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    connectToFcm()
+  }
+  // [END connect_on_active]
+  
+  // [START disconnect_from_fcm]
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    FIRMessaging.messaging().disconnect()
+    
+  }
+  // [END disconnect_from_fcm]
 }
 
 // [START ios_10_message_handling]
 @available(iOS 10, *)
 extension AppDelegate : UNUserNotificationCenterDelegate {
+  
+  // Receive displayed notifications for iOS 10 devices.
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+                              withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
     
-    // Receive displayed notifications for iOS 10 devices.
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        // Change this to your preferred presentation option
-        completionHandler([.alert,.badge])
-    }
+    // Change this to your preferred presentation option
+    completionHandler([.alert,.badge])
+  }
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-        
-        completionHandler()
-    }
+    completionHandler()
+  }
 }
 // [END ios_10_message_handling]
 
 // [START ios_10_data_message_handling]
 extension AppDelegate : FIRMessagingDelegate {
-    // Receive data message on iOS 10 devices while app is in the foreground.
-    func applicationReceivedRemoteMessage(_ remoteMessage: FIRMessagingRemoteMessage) {
-        
-    }
+  // Receive data message on iOS 10 devices while app is in the foreground.
+  func applicationReceivedRemoteMessage(_ remoteMessage: FIRMessagingRemoteMessage) {
+    
+  }
 }
 // [END ios_10_data_message_handling]
 
